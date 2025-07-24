95bef53f53163208323f5acdf924e0f14ad00abe
w3rr0 <konradmateja65@gmail.com>
2025-07-24 12:38:38 +0200

    auto refresh all rows after time (minutes) changed

--- content.py
+++ content.py
@@ -19,30 +19,42 @@ class Content(QObject):
         self.minute_monitor.start()
 
     def _check_minute_change(self):
-            """Metoda wywoływana przez QTimer co sekundę, sprawdza zmianę minuty."""
-            current_minute = datetime.now().minute
-            if current_minute != self._last_minute:
-                self._last_minute = current_minute
-                print(f"Minuta się zmieniła! Czas: {datetime.now().strftime('%H:%M')}")
+        """Metoda wywoływana przez QTimer co sekundę, sprawdza zmianę minuty."""
+        current_minute = datetime.now().minute
+        if current_minute != self._last_minute:
+            self._last_minute = current_minute
+            print(f"Minuta się zmieniła! Czas: {datetime.now().strftime('%H:%M')}")
 
-                self.add_time(0)
-                self.content_to_update.emit(self.get_content())
+            self._update_content()
+            self.content_to_update.emit(self.get_content())
+
+
+    def _update_content(self):
+        """Automatycznie aktualizuje zawartość content na podstawie jego zawartości"""
+        for row in range(len(self.selected)):
+            if self.selected[row] == "Time":
+                self.add_time(row)
+            elif self.selected[row] == "Date":
+                self.add_date(row)
 
 
     def add_time(self, row: int):
         time = datetime.now()
         time = time.strftime("%H:%M")
         self.content[row] = time
+        self.selected[row] = "Time"
 
 
     def add_date(self, row: int):
         date = datetime.now()
         date = date.strftime("%d.%m.%Y")
         self.content[row] = date
+        self.selected[row] = "Date"
 
 
     def clear_row(self, row: int):
         self.content[row] = ""
+        self.selected[row] = "Not Selected"
 
 
     def get_content(self):
