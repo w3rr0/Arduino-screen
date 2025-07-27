3c5a2011727418b3e2326d680e71698083f4d803
w3rr0 <konradmateja65@gmail.com>
2025-07-27 17:11:16 +0200

    add .ino file for arduino

---
+++ arduino-screen.ino
@@ -0,0 +1,44 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+// adres I2C (najczęściej 0x27 lub 0x3F), szerokość 16, wysokość 2
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() {
+  Serial.begin(9600);
+
+  lcd.init();              // Inicjalizacja LCD
+  lcd.backlight();         // Włączenie podświetlenia
+  lcd.setCursor(0, 0);     // Ustaw kursor na początku pierwszej linii
+  lcd.print("Disconnected");
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    String msg = Serial.readString();
+
+    // Wyczyść ekran przed wyświetleniem nowej wiadomości
+    lcd.clear();
+
+    if (msg.length() > 16) {
+      // Pobierz pierwszą część wiadomości (16 znaków)
+      String firstLine = msg.substring(0, 16);
+      
+      // Pobierz resztę wiadomości
+      String secondLine = msg.substring(16);
+
+      // Wyświetl pierwszą część na górnej linijce
+      lcd.setCursor(0, 0);
+      lcd.print(firstLine);
+
+      // Wyświetl drugą część na dolnej linijce
+      lcd.setCursor(0, 1);
+      lcd.print(secondLine);
+
+    } else {
+      // Jeśli wiadomość jest krótka, wyświetl ją normalnie
+      lcd.setCursor(0, 0);
+      lcd.print(msg);
+    } 
+  }
+}
